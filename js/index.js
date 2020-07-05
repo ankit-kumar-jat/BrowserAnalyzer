25c093815fda0b511ee88f49d540b7b9152afc7c
ankit-kumar-jat <ankjat066@gmail.com>
2020-07-05 11:10:29 +0530

    some update in index.html and contactus.html  Changes to be committed: 	deleted:    BingSiteAuth.xml 	modified:   aboutus.html 	modified:   analyzer.html 	new file:   assets/other/undraw_Hello_qnas.svg 	renamed:    contectus.html -> contactus.html 	modified:   css/index.css 	modified:   index.html 	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
 document.getElementById("myBtn").addEventListener("click", function (){
     document.location = "analyzer.html";
 });
+function addRemove(){
+    if (window.innerWidth < 992){
+        document.querySelector("#welcome-note1").style.display = "block";
+        document.querySelector("#welcome-note2").style.display = "block";
+        //document.getElementById("hello-img").style.display = "none";
+        //document.getElementById("welcome").classList.remove("col-6");
+        //document.getElementById("welcome-grid").classList.remove("row");
+    }else{
+        document.querySelector("#welcome-note1").style.display = "inline";
+        document.querySelector("#welcome-note2").style.display = "inline";
+        //document.getElementById("hello-img").style.display = "flex";
+        //document.getElementById("welcome").classList.add("col-6");
+        //document.getElementById("welcome-grid").classList.add("row");
+    }
+}
+addRemove();
+window.addEventListener("resize", addRemove);
